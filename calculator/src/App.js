44ba2aa1bf3753fb4d92a454c219f1ba735a3832
deepk09 <kbdeepak09@gmail.com>
2023-10-11 01:19:29 +0530

    initial skelton

---
+++ calculator/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='buttons'>
+        <div className='screen'>
+          <div className='previous'>123*34</div>
+          <div className='current'>123*34</div>
+        </div>
+        <button className='span_two_ac'>AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>x</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>.</button>
+        <button>0</button>
+        <button className='span_two_eq'>=</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
